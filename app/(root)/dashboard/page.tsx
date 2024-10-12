2ccece24a0315f5cdcbf5b9c8c5550199ee03f76
Swapnil Kotkar <swapnilkotkar793@gmail.com>
2024-10-12 16:22:06 +0530

    added landing page and dashbaord page

---
+++ app/(root)/dashboard/page.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+import type { Metadata } from "next";
+import HomePage from "@/components/HomePage";
+
+//working CASE 1
+// export const metadata: Metadata = {
+// 	title: {
+// 		absolute: "Home page",
+// 	},
+// };
+
+//working CASE 2 -- This case adds `Home - User Authentication App` on title tag, because `User Authentication App` is mentioned on layout page as a title tag
+export const metadata: Metadata = {
+	title: "Home",
+};
+
+const Page = () => {
+	return (
+		<div className="h-full">
+			<HomePage />
+		</div>
+	);
+};
+
+export default Page;
